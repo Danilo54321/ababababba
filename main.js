83f8b5ecbe6c7312e04fa0cd0298b66c0f43a024
Danilo54321 <149183805+Danilo54321@users.noreply.github.com>
2025-06-11 09:52:30 -0300

    rtr

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ for(let i=0;i <botoes.length;i++){
         botoes[i].classList.add("ativo");
     }
 
-    const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
@@ -23,5 +23,13 @@ for (let i = 0; i < botoes.length; i++) {
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+//Código omitido
 }
 }
